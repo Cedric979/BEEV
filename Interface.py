57bcd73e6b6bb4c46a45a373c380738ad863fccd
Cedric979 <90187909+Cedric979@users.noreply.github.com>
2021-12-15 14:49:37 +0000

    Update Interface

--- Interface.py
+++ Interface.py
@@ -39,10 +39,10 @@ import random as rd
 df = pd.read_csv("Beev Electric Vehicle Specs Data.csv")
 df['Main Price'].fillna(62000.0, inplace = True)
 df['Range (km)'].fillna(235.0, inplace = True)
-df_new = pd.DataFrame(zip(df['Full Name'],df['Brand'], df['Model'], df['Main Price'], df['Category']))
-df_new.rename(columns = {0:'Full Name', 1:'Brand', 2:'Model', 3:'Main Price',4: 'Category'}, inplace = True)
+df_new = pd.DataFrame(zip(df['Full Name'],df['Main Price'],df['Range (km)'], df['Category']))
+df_new.rename(columns = {0:'Full Name', 1:'Price',2:'Range',3:'Category'}, inplace = True)
 df_model = df_new
-df_model['Main Price'] = df_model['Main Price'].apply(lambda item: item + 6000)
+df_model['Price with Incentive'] = df_model['Price'].apply(lambda item: item - 6000)
 
 #distanceKNN.kneighbors([[60000]], 3, return_distance = False)
 #df[['Full Name', 'Main Price']].iloc[[148, 16, 57]]
@@ -62,38 +62,32 @@ label1 = "please select the category of the car you would like"
 category_choosen = st.selectbox(label1,categories)
 #st.write(category_choosen)
 
-#Filtering before fitting the model to have NN macthing the user choice
-if category_choosen!= "ALL":
-    X = df_model[df_model['Category'] == category_choosen][['Price', 'Range']]
-else: 
-    X = df_model[['Price', 'Range']]       
+     
 
-distanceKNN = NearestNeighbors(n_neighbors=5).fit(X)
 
 #Asking the user the Price and the Range
 # Add a slider to the sidebar:
-range_slider = st.sidebar.slider('Select a desired range in Kilometers',200.0, 800.0, (25.0, 75.0))
-price_slider = st.sidebar.slider('Select a desired price for the EV car',10000.0, 100000.0, (25.0, 75.0))
+range_slider = st.sidebar.slider('Select a desired range in Kilometers',200, 800, (250))
+price_slider = st.sidebar.slider('Select a desired price for the EV car',10000, 100000, (15000))
 
-
+X = pd.DataFrame()
 #Define the validation button
-if st.button('Get info'):
-    
-  st.write(value_one)
-  #  for i in range(1, 1000): #len(df_lyrics_final['Lyric'])):
-   #   
-    #    lyrics_comparator(punct(lyrics_one), punct(df_lyrics_final['Lyric'][i]))
-     #   final_dict[i] = [max_similar,similar_w_unique, similar_w_not_unique]
+if st.button('Get EV Cars recommendation'):
+    distanceKNN_cars = NearestNeighbors()
+    result = np.array([])
+    if category_choosen != "ALL":
+        X = df_model[df_model['Category'] == category_choosen][['Price', 'Range']].copy()
+        distanceKNN_cars = NearestNeighbors(n_neighbors=5).fit(X)
+    else: 
+        X = df_model[['Price','Range']].copy() 
+        
+    #st.write(X['Category'])    
+        distanceKNN_cars = NearestNeighbors(n_neighbors=5).fit(X)
+    result = distanceKNN_cars.kneighbors([[price_slider,range_slider]], 5, return_distance = False)
+    #st.write(result)
+    for i in range(5):
+        st.write(df_model[['Full Name', 'Price','Range','Category','Price with Incentive']].iloc[[result[0][i]]])
 
-  #  lyrics_report(punct(lyrics_one), punct(df_lyrics_final['Lyric'][dict_sum(final_dict)]))
-   # st.write(df_lyrics_final.iloc[dict_sum(final_dict)][['Artist','Song']]) #veribal 
-    #link1 = df_lyrics_final.iloc[dict_sum(final_dict)][['SLink']][0]
-    #st.write(type(link1))
-
-
-    #st.write(link1)
-   # url = f"https://www.vagalume.com.br{link1}"
-   # st.write(url)
 
 
 
