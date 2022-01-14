6b05ab06aa255cd0b87cabed1d9d8767550097e0
Cedric979 <90187909+Cedric979@users.noreply.github.com>
2022-01-14 18:57:50 +0000

    update
    letter_csv with dataframe columns saved

--- Interface.py
+++ Interface.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 #LIBRARIES
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd # data processing
 #Graphics
 #import matplotlib.pyplot as plt
 #import seaborn as sns
