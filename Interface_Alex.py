0f22e21e364e7cad9ee2199080b06e2011063c6c
Alexandre Gomes <alexandresantosgomes@hotmail.com>
2022-01-25 12:11:47 +0000

    Update Interface_Alex.py
    Trying for docker

--- Interface_Alex.py
+++ Interface_Alex.py
@@ -32,29 +32,29 @@ import gspread
 #global link1
 
 #Changing the background with an image that has to be in the same folder
-import base64
-main_bg = "../app/st_back_main3.jpeg"
-main_bg_ext = "jpeg"
+# import base64
+# main_bg = "st_back_main3.jpeg"
+# main_bg_ext = "jpeg"
 
-side_bg = "../app/st_back_slider.jpeg"
-side_bg_ext = "jpeg"
+# side_bg = "st_back_slider.jpeg"
+# side_bg_ext = "jpeg"
 
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-        background-size: 100% 100%
-    }}
-   .sidebar.sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
-        background-size: 100% 100%
-    }}
-    </style>
+# st.markdown(
+#     f"""
+#     <style>
+#     .reportview-container {{
+#         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+#         background-size: 100% 100%
+#     }}
+#    .sidebar.sidebar-content {{
+#         background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+#         background-size: 100% 100%
+#     }}
+#     </style>
     
-    """,
-    unsafe_allow_html=True
-)
+#     """,
+#     unsafe_allow_html=True
+# )
 
 #importing the googlesheet document named fuel prices
 # C:\Users\Utilizador\Documents\GitHub\BEEV\Interface_Alex.py
