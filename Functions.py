fdea3c20dd1b27b1b9477ef6c577fbb111841a1a
Cedric979 <90187909+Cedric979@users.noreply.github.com>
2022-02-11 16:37:33 +0000

    Update Functions.py
    Change function with dico of urls outside

--- Functions.py
+++ Functions.py
@@ -9,11 +9,11 @@ from Libraries import *
 
 # %%
 #Building the Data for maintenance_costs. We divide it by 10000 because we had too many 0's
-def load_df(url_dico):
-    url_dico = {'fuel_prices': "https://docs.google.com/spreadsheets/d/1M_e1ENe40v-G_HYYH7YTZT5yPMoxgk36FFNamtg12f8/edit?usp=sharing",
+url_dico = {'fuel_prices': "https://docs.google.com/spreadsheets/d/1M_e1ENe40v-G_HYYH7YTZT5yPMoxgk36FFNamtg12f8/edit?usp=sharing",
            'title_tax_cv': "https://docs.google.com/spreadsheets/d/1cOj98R9fGT89rG4-TxAPIgOrvDbrzxlLPd4Y5mUBD0g/edit?usp=sharing",
            'bonus_malus': "https://docs.google.com/spreadsheets/d/1RDIMbTGE3TBU4SXbRNKiqKQFakf0grVGKKLfu9L9dS4/edit?usp=sharing",
            'maintenance_costs': "https://docs.google.com/spreadsheets/d/1Hlhp4ubS-JFgYYx1S9oeL5A_011sSxvKzWiuS3bLqV8/edit?usp=sharing"}
+def load_df(url_dico = url_dico):
     gc = gspread.service_account('app/beev-341010-50a2328de8c9.json')#The json key has to be defined by a google account in google API
     for key, url in url_dico.items():
         sht1 = gc.open_by_url(url)
